d84230034a7adf4299c2962f2fab435e0369b2ce
mugaaaaa <yunfaimok59@gmail.com>
2026-02-06 07:02:00 +0800

    feat: adjust banner height

--- src/config.ts
+++ src/config.ts
@@ -12,13 +12,13 @@ export const siteConfig: SiteConfig = {
 	subtitle: "记录代码、语言学和生活",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 325, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 120, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: true,
 		src: "assets/images/basic/ptilopsis_10.jpeg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		position: "top", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
 			text: "", // Credit text to be displayed
